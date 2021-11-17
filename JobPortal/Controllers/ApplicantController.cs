620c037bb00872d6ecec97a289caa5a665594593
Ashin Sajan <vrsprojects@bourntec.com>
2021-11-18 03:31:24 +0530

    New base Project -Ashin

---
+++ JobPortal/Controllers/ApplicantController.cs
@@ -0,0 +1,55 @@
+﻿using JobPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class ApplicantController : Controller
+    {
+        private DBOperationDataContext _context;
+
+        public ApplicantController() 
+        {
+            _context = new DBOperationDataContext();
+        }
+        // GET: Applicant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login() 
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserModel userobj)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginDetails = _context.user_accounts.Where(u => u.email_id == userobj.email_id && u.password == userobj.password &&u.user_type == "jobseeker").FirstOrDefault();
+                if (loginDetails != null)
+                {
+                    Session["User"] = userobj.email_id;
+                    Session["UserId"] = userobj.id;
+
+                    return RedirectToAction("Index","Applicant");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            else 
+            { 
+                return View(); 
+            }
+
+        }
+    }
+}
---
+++ JobPortal/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobPortal.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Enter Email Address")]
+        public string email_id { get; set; }
+
+        [Required(ErrorMessage = "Enter Password")]
+        public string password { get; set; }
+
+        public string phone_number { get; set; }
+
+        public string user_type { get; set; }
+
+
+    }
+}
