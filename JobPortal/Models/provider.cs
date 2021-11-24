5c9506e7f2cd05001e89e2c136812eac19bf2fec
Jeena Roy <vrsprojects@bourntec.com>
2021-11-24 12:04:15 +0530

    layout

--- JobPortal/Models/provider.cs
+++ JobPortal/Models/provider.cs
@@ -15,10 +15,10 @@ namespace JobPortal.Models
         [Display(Name = "Posted Id")]
         public int posted_by_id { get; set; }
 
-        [Display(Name = "Jobtype Id")]
+        [Display(Name = "Jobtype")]
         public int job_type_id { get; set; }
 
-        [Display(Name = "Company Id")]
+        [Display(Name = "Company")]
         public int company_id { get; set; }
 
         [Display(Name = "Created Date")]
@@ -27,7 +27,7 @@ namespace JobPortal.Models
         [Display(Name = "Job Description")]
         public string job_description { get; set; }
 
-        [Display(Name = "Joblocation Id")]
+        [Display(Name = "Joblocation")]
         public int job_location_id { get; set; }
 
         [Display(Name = "Is active")]
