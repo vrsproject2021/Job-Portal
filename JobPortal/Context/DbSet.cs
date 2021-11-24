98268fa494c9c2faadac11478bdf174c7f043514
Karthik Vasudevan <vrsprojects@bourntec.com>
2021-11-24 12:08:53 +0530

    layout

---
+++ JobPortal/Context/DbSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JobPortal.Context
+{
+    public class DbSet<T>
+    {
+        internal object ToList()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Tolist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- JobPortal/Controllers/AdminController.cs
+++ JobPortal/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JobPortal.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,17 +9,86 @@ namespace JobPortal.Controllers
 {
     public class AdminController : Controller
     {
+
+        userContext db = new userContext();
+
         // GET: Admin
         public ActionResult Index()
+        {
+            var users = db.users.Tolist();
+            return View();
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int id)
         {
             return View();
         }
 
-
-
-        public ActionResult Users()
+        // GET: Admin/Create
+        public ActionResult Create()
         {
             return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
