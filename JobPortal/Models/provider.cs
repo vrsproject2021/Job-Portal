a8a94c6fab6030393f5c4955d2a4623901f57724
Jeena Roy <vrsprojects@bourntec.com>
2021-12-16 17:54:34 +0530

    jobpost

--- JobPortal/Models/provider.cs
+++ JobPortal/Models/provider.cs
@@ -9,6 +9,7 @@ namespace JobPortal.Models
     public class provider
     {
         internal string company_name;
+        internal string job;
 
         public int id { get; set; }
 
@@ -20,6 +21,8 @@ namespace JobPortal.Models
         [Display(Name = "Jobtype")]
         public int job_type_id { get; set; }
 
+        public string Job { get; set; }
+
         [Required(ErrorMessage = "Enter Your Company")]
         [Display(Name = "Company")]
         public int company_id { get; set; }
@@ -29,6 +32,12 @@ namespace JobPortal.Models
         [Display(Name = "Created Date")]
         public DateTime created_date { get; set; }
 
+        [Required(ErrorMessage = "Enter Your Created date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTime end_date { get; set; }
+
+
         [Required(ErrorMessage = "Enter Your Job description")]
         [Display(Name = "Job Description")]
         public string job_description { get; set; }
@@ -37,7 +46,16 @@ namespace JobPortal.Models
         [Display(Name = "Joblocation")]
         public int job_location_id { get; set; }
 
-        [Display(Name = "Is active")]
-        public char is_active { get; set; }
+        [Display(Name = "Minimum Salary")]
+        public int min_salary { get; set; }
+        
+        [Display(Name = "Maximum Salary")]
+        public int max_salary { get; set; }
+
+
+public Boolean is_active { get; set; }
+
+
+
     }
 }
